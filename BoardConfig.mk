c00859379a29ebfc1990ed2dbe1971c5f4ea0fbd
Dan Trifan <jimsth@gmail.com>
2014-08-07 08:24:21 +0300

    hammerhead: compile the kernel with linaro 4.9
    Change-Id: I51b78321353556705a2efff679cf6fdc4755be06
    Signed-off-by: Dan Trifan <jimsth@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -104,6 +104,7 @@ BOARD_SEPOLICY_DIRS := \
 # Define kernel config for inline building
 TARGET_KERNEL_CONFIG := hammerhead_defconfig
 TARGET_KERNEL_SOURCE := kernel/lge/hammerhead
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := linaro-4.9
 
 # The list below is order dependent
 BOARD_SEPOLICY_UNION := \
