ff36f25480660b96e0cbb5af8be891917515b0ce
DHOMD <DHO@dho.im>
2014-09-14 11:03:12 -0400

    Revert "hammerhead: Make audio not suck anymore."
    This reverts commit 9d7d8ff2ed4d0a08453b7eeac557cfaeedbea28e.
    
    Change-Id: Ib9b0f5fb7d83c203a154322785fdf07db3bffba2
    Signed-off-by: DHOMD <DHO@dho.im>

--- device.mk
+++ device.mk
@@ -300,11 +300,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.audio.fluence.speaker=false \
     af.resampler.quality=4 \
     audio.offload.buffer.size.kb=32 \
-    audio.offload.gapless.enabled=true \
-    av.offload.enable=true \
-    av.streaming.offload.enable=true \
-    audio.offload.pcm.enable=true \
-    audio.offload.24bit.enable=1
+    audio.offload.gapless.enabled=false \
+    av.offload.enable=true
 
 # Setup custom emergency number list based on the MCC. This is needed by RIL
 PRODUCT_PROPERTY_OVERRIDES += \
