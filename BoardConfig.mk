7e782255cb5fa3713d3102759b82895e2c21b9e6
Dan Trifan <jimsth@gmail.com>
2014-10-01 08:28:34 +0300

    hammerhead: update qc perf variable name
    Change-Id: Icdad62fdffff8046c082887fb100b63d0a91aa27
    Signed-off-by: Dan Trifan <jimsth@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -136,7 +136,7 @@ COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
 # Workaround for broken video recording when compiling thumb with -Os on FLO and HH
 AN_ASSHAT_HAS_BROKEN_MY_CAMERA_SOURCE := true
 # compile with qcom proprietary static libraries
-WITH_QC_PERF := true
+TARGET_HAVE_QC_PERF := true
 
 # Hardware
 BOARD_HARDWARE_CLASS := device/lge/hammerhead/cmhw
